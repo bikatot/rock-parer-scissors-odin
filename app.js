c20662cb0c2de04e02cb436ed86ef190cbf1caf8
bikatot <vladx002@gmail.com>
2022-06-19 16:46:55 +0300

    Create a function for outcome calculation

--- app.js
+++ app.js
@@ -2,12 +2,38 @@ function computerPlay() {
   let cpuChoice = Math.random() * 3;
   console.log(cpuChoice);
   if (cpuChoice <= 1) {
-    return "Rock";
+    return  "rock";
   } else if (cpuChoice <= 2) {
-    return "Paper";
+    return "paper";
   } else {
-    return "Scissors";
+    return "scissors";
   }
 }
 
-console.log(computerPlay())
+function playRound(playerSelection, computerSelection) {
+  playerSelection = playerSelection.toLowerCase();
+  console.log(playerSelection);
+  console.log(computerSelection);
+  if (playerSelection === computerSelection) {
+    return "That's a tie!";    
+  } else if (playerSelection === "rock" && computerSelection === "paper") {
+    return "You lose! Paper beats Rock";
+  } else if (playerSelection === "paper" && computerSelection === "scissors") {
+    return "You lose! Scissors beat Paper ";
+  } else if (playerSelection === "scissors" && computerSelection === "rock") {
+    return "You lose! Rock beats Scissors";
+  } else if (playerSelection === "rock" && computerSelection === "scissors") {
+    return "You win! Rock beats Scissors";
+  } else if (playerSelection === "paper" && computerSelection === "rock") {
+    return "You win! Paper beats Rock";
+  } else if (playerSelection === "scissors" && computerSelection === "paper") {
+    return "You win! Scissors beat Paper ";
+  } else {
+    return "Something went wrong";
+  }
+}
+
+
+const playerSelection = prompt("Enter your handsign")
+const computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection));
