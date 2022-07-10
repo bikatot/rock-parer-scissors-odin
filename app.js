45c7a04e7a0c3ba9dec40bd08fbd586c366a18d5
bikatot <vladx002@gmail.com>
2022-07-10 17:41:11 +0300

    Alter the program to be played with buttons

--- app.js
+++ app.js
@@ -10,12 +10,33 @@ function computerPlay() {
   }
 }
 
-let result = ""
+let result = "";
+let playerScore = 0;
+let computerScore = 0;
+let buttons = document.querySelector("#buttonsContainer");
+let currentRoundResult = document.querySelector("#roundResult");
+let gameResult = document.querySelector("#gameResult");
+let playerChoice = document.querySelector("#playerChoice");
+let computerChoice = document.querySelector("#CPUChoice");
 
-function playRound(playerSelection, computerSelection) {  
-  playerSelection = playerSelection.toLowerCase();
-  console.log(playerSelection);
-  console.log(computerSelection);
+const rockButton = document.querySelector("#rock");
+const paperButton = document.querySelector("#paper");
+const scissorsButton = document.querySelector("#scissors");
+
+const playerScoreHTML = document.querySelector("#playerScore");
+const computerScoreHTML = document.querySelector("#CPUScore");
+
+
+rockButton.addEventListener("click", () => game("rock", computerPlay()));
+paperButton.addEventListener("click", () => game("paper", computerPlay()));
+scissorsButton.addEventListener("click", () => game("scissors", computerPlay()));
+
+
+let game = function playRound(playerSelection, computerSelection) {
+  playerSelection = playerSelection.toLowerCase();  
+  currentRoundResult.textContent = "Round result: ";
+  computerChoice.textContent = "Computer chooses: ";
+  playerChoice.textContent = "You choose: ";
   if (playerSelection === computerSelection) {
     result = "That's a tie!";    
   } else if (playerSelection === "rock" && computerSelection === "paper") {
@@ -32,32 +53,32 @@ function playRound(playerSelection, computerSelection) {
     result = "You win! Scissors beat Paper ";
   } else {
     result = "Something went wrong";
-  } return result;
-}
-
-function game() {
-  let playerScore = 0;
-  let computerScore = 0;
-  for (let i = 0; i < 5; i++) {
-    const playerSelection = prompt("Enter your handsign")
-    const computerSelection = computerPlay();
-    playRound(playerSelection, computerSelection)
+  }  currentRoundResult.textContent += result;
     if (result.includes("win")) {
-      playerScore += 1;
+    (playerScore += 1);
     } else if (result.includes("lose")) {
       computerScore += 1;
-    } 
-    console.log(result);
-    console.log(playerScore);
-    console.log(computerScore);
-  } 
-  if (playerScore > computerScore) {
-    return "Congratulations! You have won the game"
-  } else if (playerScore < computerScore) {
-    return "You've lost the game"
-  } else {
-    return "The game has ended in a draw  "
-  }
-}
+    };
+  computerChoice.textContent += computerSelection;
+  playerChoice.textContent += playerSelection;
+  playerScoreHTML.textContent = playerScore;
+  computerScoreHTML.textContent = computerScore;
+  
+  if (playerScore === 5) {
+    gameResult.textContent = "You have won the game";
+  } else if (computerScore === 5) {
+    gameResult.textContent = "You have lost the game";
+  };
+  if (playerScore === 5 || computerScore === 5) {
+    buttons.remove();
+    let playAgain = document.createElement("button");
+    playAgain.textContent = "Play again";
+    gameResult.appendChild(playAgain);
+    playAgain.addEventListener("click", refreshPage);
+  }; 
+    
+};
 
-console.log(game());
+const refreshPage = () => {
+  location.reload();
+};
